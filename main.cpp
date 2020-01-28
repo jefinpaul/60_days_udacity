0a9670e50467a72202f144365285339fcc64b04c
Jefin <46289554+jefinpaul@users.noreply.github.com>
2020-01-28 23:49:01 +0530

    Day 28
    Takes input n and reverse the order hackerrank

--- main.cpp
+++ main.cpp
@@ -1,50 +1,10 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
-
-using namespace std;
-
-class employee
-{
-    char name[30];
-    float age;
-  public: 
-  void getdata(void);
-  void putdata(void);
-};
-void employee :: getdata(void)
-{
-    cout<<"Enter name";
-    cin>>name;
-    cout<<"enter age";
-    cin>>age;
+int main() {
+    int N,i=0;
+    std::cin>>N;
+    int *A = new int[N];
+    while(std::cin>>A[i++]);
+    while(std::cout<<A[--N]<<' ' && N);
+    delete[] A;
+    return 0;
 }
-void employee :: putdata(void)
-{
-    cout<<"name"<<name;
-    cout<<"age"<<age;
-}
-int main()
-{
-    employee manager[1];
-    employee foreman[2];
-     for(int i=0;i<2;i++)
-     {
-         manager[i].getdata();
-         manager[i].putdata();
-     }
-     for(int j=0;j<3;j++)
-     {
-         foreman[j].getdata();
-         foreman[j].putdata();
-     }
-}
-  
-
-    
