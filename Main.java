2eab20dfe06b9761f79e810263b819347cee866b
Jefin <46289554+jefinpaul@users.noreply.github.com>
2020-03-12 20:23:08 +0530

    To find square root in java
    EAsy

---
+++ Main.java
@@ -0,0 +1,13 @@
+/*code to find square root*/
+import java.lang.Math;
+class Main
+{
+    public static void main(String args[])
+    {
+        double x =5;
+        double y;
+        y = Math.sqrt(x);
+        System.out.print("y="+y);    
+    }
+    
+}
