3c206dd981d144e308898ec4f40b07b6e61c7762
Jefin Paul <46289554+jefinpaul@users.noreply.github.com>
2020-04-15 16:52:54 +0530

    Update dictonary datacamp.py

--- dictonary datacamp.py
+++ dictonary datacamp.py
@@ -16,3 +16,21 @@ europe['italy']= data
 
 # Print europe
 print(europe)
+
+Use Boolean conditions to subset for rows in 2010 and 2011, and print the results.
+Set the index to the date column.
+Use .loc[] to subset for rows in 2010 and 2011.
+Use .loc[] to subset for rows from Aug 2010 to Feb 2011.
+
+
+# Use Boolean conditions to subset temperatures for rows in 2010 and 2011
+print(temperatures[(temperatures["date"] >= "2010") & (temperatures["date"] < "2012")])
+
+# Set date as an index
+temperatures_ind = temperatures.set_index("date")
+
+# Use .loc[] to subset temperatures_ind for rows in 2010 and 2011
+print(temperatures_ind.loc["2010":"2011"])
+
+# Use .loc[] to subset temperatures_ind for rows from Aug 2010 to Feb 2011
+print(temperatures_ind.loc["2010-08":"2011-02"])
